dff03e6b4fdffe848d2a996383c12543e3f495c9
unknown <bati.mayer@gmail.com>
2022-10-26 20:00:25 +0300

    added dynemic html in js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello tea shop")
+const productsContainer = document.getElementById("products-container")
+const blackTea = document.createElement("div")
+const greenTea = document.createElement("div")
+
+blackTea.innerHTML = "Black Tea"
+greenTea.innerHTML = "Green Tea"
+
+blackTea.classList.add("product")
+greenTea.classList.add("product")
+
+productsContainer.appendChild(blackTea)
+productsContainer.appendChild(greenTea)
